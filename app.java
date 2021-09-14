cb99b78d16cbfd33b4fa43924cd8bf8720307b1c
praveenneeru <praveenneeru2013@gmail.com>
2021-09-14 23:58:36 +0530

    added by pk

--- app.java
+++ app.java
@@ -5,3 +5,9 @@ class job
 	int deadline; //deadline
 	int profit; //profit of the job
 };
+bool mycompare(job *x,job *y)//boolean function
+{
+	//sort as per decreasing profite
+    return x->profit>y->profit; 
+}
+sort(obj,obj+n,mycompare);
